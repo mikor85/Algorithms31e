7e65446194fd29f8234408dbd68d078f005ac739
mikor85 <cormakoff@gmail.com>
2022-09-04 11:35:38 +0300

    Lesson5, MergeString added.

---
+++ src/main/java/Algorithms/lessonfive/MergeString.java
@@ -0,0 +1,26 @@
+package Algorithms.lessonfive;
+
+public class MergeString {
+    public static void main(String[] args) {
+        String one = "abcde";  // adbgckde
+        String two = "dgk";
+        System.out.println(new MergeString().mergeString(one, two));
+    }
+
+    private String mergeString(String one, String two) {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < one.length() + two.length(); i++) {
+            if (i < one.length() && i < two.length()) {
+                result.append(one.charAt(i)).append(two.charAt(i));
+                continue;
+            }
+            if (i < one.length()) {
+                result.append(one.charAt(i));
+            }
+            if (i < two.length()) {
+                result.append(two.charAt(i));
+            }
+        }
+        return result.toString();
+    }
+}
