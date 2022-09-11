5d9fca4897f740a155fb138271cb5b9e256452b3
mikor85 <cormakoff@gmail.com>
2022-09-11 15:21:32 +0300

    QuickSortHomework has been changed

--- src/main/java/Algorithms/homework_5/QuickSortHomework.java
+++ src/main/java/Algorithms/homework_5/QuickSortHomework.java
@@ -12,8 +12,9 @@ public class QuickSortHomework {
         // 4. Выбран первый или последний элемент в качестве опорного.
 
         int[] array = {34, 56, 22, 78, 9, 3, 5, 15};
-        quickSort(array, 0, array.length - 1);
-        System.out.println(Arrays.toString(array));
+        int[] arrayTwo = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        quickSort(arrayTwo, 0, arrayTwo.length - 1);
+        System.out.println(Arrays.toString(arrayTwo));
         System.out.println("----------");
 
     }
@@ -22,23 +23,34 @@ public class QuickSortHomework {
         if (start < end) {  // базовый случай
             int indexPivot = getIndexPivot(array, start, end);
             quickSort(array, start, indexPivot - 1);
-            quickSort(array, indexPivot + 1, end);
+            quickSort(array, indexPivot/* + 1*/, end);
         }
     }
 
     private static int getIndexPivot(int[] array, int start, int end) {
-        int leftIndex = start;
-        int rightIndex = end;
-
         // оптимальный случай
         //int pivot = array[start + (end - start) / 2];
 
         // худший случай 1 - выбран первый опорный элемент
-        int pivot = array[start];
+        //int pivot = array[start];
 
         // худший случай 2 - выбран последний опорный элемент
-        //int pivot = array[end];
+        int pivot = array[end];
 
+        // =================================================
+        if (pivot == array[end]){
+            int indexPivot = start;
+            for (int i = start; i < end; i++) {
+                if (array[i] <= pivot) {
+                    swap(array, indexPivot, i);
+                    indexPivot++;
+                }
+            }
+            swap(array, indexPivot, end);
+            return indexPivot;
+        }
+        int leftIndex = start;
+        int rightIndex = end;
         while (leftIndex <= rightIndex){
 
             while (array[leftIndex] < pivot){
@@ -53,7 +65,6 @@ public class QuickSortHomework {
                 rightIndex--;
             }
         }
-
         return leftIndex;
     }
 
