21837a59b520d85f7f5c5b0b709b3a01a584433e
mikor85 <cormakoff@gmail.com>
2022-09-16 21:02:31 +0300

    CountDuplicates_Task3 - a binary search for first and last Index has been added.

--- src/main/java/Algorithms/homework_3/CountDuplicates_Task3.java
+++ src/main/java/Algorithms/homework_3/CountDuplicates_Task3.java
@@ -8,13 +8,65 @@ package Algorithms.homework_3;
 public class CountDuplicates_Task3 {
     public static void main(String[] args) {
 
-        CountDuplicates_Task3 countDuplicates = new CountDuplicates_Task3();
         int x = 2;
-        int[] arr = {1, 1, 2, 2, 2, 2, 3,};
-        System.out.println(countDuplicates.countNumberDuplicatesInArray(x, arr));
+        int[] arr = {1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 7, 7};
+
+        int firstIndex = findFirstOccurrenceIndex(arr, x);
+        int lastIndex = findLastOccurrenceIndex(arr, x);
+        if (firstIndex == -1 || lastIndex == -1) {
+            System.out.printf("Элемент %d не найден в массиве", x);
+        } else {
+            if (lastIndex - firstIndex + 1 == 2 ||
+                    lastIndex - firstIndex + 1 == 3 ||
+                    lastIndex - firstIndex + 1 == 4) {
+                System.out.printf("Элемент %d встречается в массиве %d раза", x, lastIndex - firstIndex + 1);
+                System.out.println();
+            } else {
+                System.out.printf("Элемент %d встречается в массиве %d раз", x, lastIndex - firstIndex + 1);
+                System.out.println();
+            }
+        }
     }
 
-    private int countNumberDuplicatesInArray(int x, int[] arr) {
+    private static int findFirstOccurrenceIndex(int[] arr, int x) {
+        int left = 0;
+        int right = arr.length - 1;
+        int result = -1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (x == arr[mid]) {
+                result = mid;
+                right = mid - 1;
+            } else if (x < arr[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return result;
+    }
+
+    private static int findLastOccurrenceIndex(int[] arr, int x) {
+        int left = 0;
+        int right = arr.length - 1;
+        int result = -1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (x == arr[mid]) {
+                result = mid;
+                left = mid + 1;
+            } else if (x < arr[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return result;
+    }
+
+    // My
+    private int countNumberDuplicatesInArrayMy(int[] arr, int x) {
         int res = 0;
         for (int i = 0; i < arr.length; i++) {
             if (x == arr[i]) {
